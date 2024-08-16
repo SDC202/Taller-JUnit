5a9a8fd0649c63c661f482e7d7a7a0e996e15ad1
SDC202 <sebas.decastro@hotmail.com>
2024-08-15 23:10:24 -0500

    Cambio a los tests con assuptions

--- src/main/java/Calculation.java
+++ src/main/java/Calculation.java
@@ -15,7 +15,6 @@ public class Calculation {
 	
 	public int findMax(int arr[]) {
 
-        //Asumiendo que el arreglo no es vacio
         int max = arr[0]; 
         for (int i = 1; i < arr.length; i++) { 
             if (max < arr[i]) 
@@ -23,22 +22,6 @@ public class Calculation {
         }
         return max; 
     }
-	
-	
-	@Test
-	 public void testFindMaxPositivos(){
-	 assertEquals(5,findMax(new int[]{1,2,3,4,5}));
-	 assertEquals(500,findMax(new int[]{100,20,300,500,50}));
-	 assertEquals(60,findMax(new int[] {60,30,20,50,40,10}));
-	 }
-	
-	@Test
-	 public void testFindMaxNegativos(){
-	 assertEquals(-1,findMax(new int[]{-1,-2,-3,-4,-5}));
-	 assertEquals(-20,findMax(new int[]{-100,-20,-300,-500,-50}));
-	 assertEquals(-10,findMax(new int[] {-60,-30,-20,-50,-40,-10}));
-	 }
-
 
 
 }
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -6,13 +6,19 @@ import org.junit.Assume;
 public class EmployeeTest {
 
     @Test
-    public void testCSWorkerInUSD_EvenMonth() {
+    public void testCSWorkerUSD_MesPar() {
+    	LocalDate currentDate = LocalDate.now();
+        int mes = currentDate.getMonthValue();
+        Assume.assumeTrue(mes % 2 == 0);
         Employee employee = new Employee(1000, "USD", 0.1f, EmployeeType.Worker);
         assertEquals(1000, employee.cs(), 0.01);
     }
     
     @Test
-    public void tetsCSWorkerInUSD_OddMonth() {
+    public void tetsCSWorkerUSD_MesImpar() {
+    	LocalDate currentDate = LocalDate.now();
+        int mes = currentDate.getMonthValue();
+        Assume.assumeTrue(mes % 2 != 0);
         Employee employee = new Employee(1000, "USD", 0.1f, EmployeeType.Worker);
         // Suponiendo que el mes es impar, debería agregar el décimo
         float expectedSalary = 1000 + (386.0f / 12 * 2);
@@ -20,7 +26,10 @@ public class EmployeeTest {
     }
 
     @Test
-    public void testCSWorkerInOtherCurrency_EvenMonth() {
+    public void testCSWorkerEUR_MesPar() {
+    	LocalDate currentDate = LocalDate.now();
+        int mes = currentDate.getMonthValue();
+        Assume.assumeTrue(mes % 2 == 0);
         Employee employee = new Employee(1000, "EUR", 0.1f, EmployeeType.Worker);
         // Restando 5% por cambio de moneda
         float expectedSalary = 1000 * 0.95f;
@@ -28,15 +37,54 @@ public class EmployeeTest {
     }
 
     @Test
-    public void testCSWorkerInOtherCurrency_OddMonth() {
+    public void testCSWorkerEUR_MesImpar() {
+    	LocalDate currentDate = LocalDate.now();
+        int mes = currentDate.getMonthValue();
+        Assume.assumeTrue(mes % 2 != 0);
         Employee employee = new Employee(1000, "EUR", 0.1f, EmployeeType.Worker);
         // Restando 5% por cambio de moneda y agregando el décimo
         float expectedSalary = 1000 * 0.95f + (386.0f / 12 * 2);
         assertEquals(expectedSalary, employee.cs(), 0.01);
     }
+    
+    @Test
+    public void testCS_SupervisorUSD_MesImpar() {
+        // Asume que el mes actual es impar
+        LocalDate currentDate = LocalDate.now();
+        int currentMonth = currentDate.getMonthValue();
+        Assume.assumeTrue(currentMonth % 2 != 0);
+
+        Employee employee = new Employee(1500, "USD", 0.1f, EmployeeType.Supervisor);
+        float expectedSalary = 1500 + 0.1f * 0.35f + (386.0f / 12 * 2);
+        assertEquals(expectedSalary, employee.cs(), 0.01);
+    }
+    
+    @Test
+    public void testCS_SupervisorUSD_MesPar() {
+        LocalDate currentDate = LocalDate.now();
+        int currentMonth = currentDate.getMonthValue();
+        Assume.assumeTrue(currentMonth % 2 == 0);
+
+        Employee employee = new Employee(1500, "USD", 0.1f, EmployeeType.Supervisor);
+        float expectedSalary = 1500 + 0.1f * 0.35f;
+        assertEquals(expectedSalary, employee.cs(), 0.01);
+    }
 
     @Test
-    public void testCSManagerInUSD_OddMonth() {
+    public void testCSManagerUSD_MesPar() {
+    	LocalDate currentDate = LocalDate.now();
+        int mes = currentDate.getMonthValue();
+        Assume.assumeTrue(mes % 2 == 0);
+        Employee employee = new Employee(2000, "USD", 0.2f, EmployeeType.Manager);
+        float expectedSalary = 2000 + 0.7f * 0.2f;
+        assertEquals(expectedSalary, employee.cs(), 0.01);
+    }
+    
+    @Test
+    public void testCSManagerUSD_MesImpar() {
+    	LocalDate currentDate = LocalDate.now();
+        int mes = currentDate.getMonthValue();
+        Assume.assumeTrue(mes % 2 != 0);
         Employee employee = new Employee(2000, "USD", 0.2f, EmployeeType.Manager);
         float expectedSalary = 2000 + 0.7f * 0.2f + (386.0f / 12 * 2);
         assertEquals(expectedSalary, employee.cs(), 0.01);
@@ -49,7 +97,7 @@ public class EmployeeTest {
     }
 
     @Test
-    public void testCalculateYearBonusManagerOtherCurrency() {
+    public void testCalculateYearBonusManagerEUR() {
         Employee employee = new Employee(2000, "EUR", 0.2f, EmployeeType.Manager);
         float expectedBonus = 2000 * 0.95f + 386.0f * 1.0f;
         assertEquals(expectedBonus, employee.CalculateYearBonus(), 0.01);
